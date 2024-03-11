13c242cdf2650e205dcaa29eb3cf91de89269dad
Palpal16 <simone.paloschi@mail.polimi.it>
2024-03-11 18:00:18 +0000

    mi sa che ho fatto un casotto

--- Dati.hpp
+++ Dati.hpp
@@ -1,2 +1,18 @@
+#ifndef DATA_H
+#define DATA_H
 
-// Cration of the struct with the data
+#include <stdio.h>
+
+// Define the struct
+
+struct Data{
+    // function wrapper f
+    // function wrapper nabla f
+    // initial conditions
+    // tolerances
+    // initial step
+    // maximal number of iterations
+    int member1;
+};
+
+#endif // DATA_H
